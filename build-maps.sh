5cc39c43ef5ba6641dac622170457476ca3e8949
EC2 Default User <ec2-user@ip-10-0-255-140.ec2.internal>
2022-06-15 22:24:35 +0000

    aa

--- build-maps.sh
+++ build-maps.sh
@@ -4,7 +4,7 @@ for dir in /worlds/*; do
 	echo $dir
 	echo `basename $dir`
 	mkdir /$root
-	/unmined/unmined-cli web render --world=/world/$root --output=/$root
+	/unmined/unmined-cli web render --world=/worlds/$root/world --output=/$root
 	mv /$root/unmined.index.html /$root/index.html
 	aws s3 sync /$root s3://minecraft-$root
 done
